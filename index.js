a55d7ceecaeb97652aeaab8b69f1aedbae3741b3
robliendo <126755451+robliendo@users.noreply.github.com>
2023-05-08 23:22:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// < -------------------------------  TIMMY -------------------------------- >
+// your first assignment is to 
+// create the logic for their checkout system that 
+// *** returns the total cost of all refills including if the customer has a subscription and/or a coupon.
+
+// If the customer has a coupon, 
+// the customer will receive a $10 discount after the subscription discount has been calculated.
+
 const timmy = {
     prescription: "acetaminophen",
     pricePerRefill: 25,
@@ -5,6 +13,17 @@ const timmy = {
     subscription: false,
     coupon: true,
 };
+
+// Timmy => "Your grand total is $65"
+
+
+// < -------------------------------  SARAH -------------------------------- >
+// your first assignment is to 
+// create the logic for their checkout system that 
+// *** returns the total cost of all refills including if the customer has a subscription and/or a coupon.
+
+// If a customer has a subscription, 
+// the customer will receive a 25% discount after the refill total has been calculated.
 
 const sarah = {
     prescription: "diphenhydramine",
@@ -14,6 +33,19 @@ const sarah = {
     coupon: false,
 }
 
+// Sarah => "Your grand total is $37.5."
+
+// < -------------------------------  ROCKY -------------------------------- >
+// your first assignment is to 
+// create the logic for their checkout system that 
+// *** returns the total cost of all refills including if the customer has a subscription and/or a coupon.
+
+// If a customer has a subscription, 
+// the customer will receive a 25% discount after the refill total has been calculated.
+
+// If the customer has a coupon, 
+// the customer will receive a $10 discount after the subscription discount has been calculated.
+
 const rocky = {
     prescription: "phenylephrine",
     pricePerRefill: 30,
@@ -21,3 +53,12 @@ const rocky = {
     subscription: true,
     coupon: true,
 }
+
+// Rocky => "Your grand total is $102.5"
+
+
+
+
+
+
+
